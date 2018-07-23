1690b4b2d6f37732226752f9464bf013a4f734c2
Billy Davidson <billy.davids52@gmail.com>
2018-07-23 15:21:19 -0400

    wheelchair access

---
+++ catmeow.rb
@@ -0,0 +1,24 @@
+class Cat
+  def name
+    @name
+  end
+
+  def preferred_food
+    @preferred_food
+  end
+
+  def preferred_food=(meowmix)
+    @preferred_food = meowmix
+
+  def meal_time
+    @meal_time
+  end
+
+  def meal_time=(time)
+    @meal_time = time
+
+  def initialize
+
+
+
+end
