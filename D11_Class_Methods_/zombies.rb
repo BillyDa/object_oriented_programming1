cfeb415b60ac8fe31c2c2f2634e15c735e460a5a
Billy Davidson <billy.davids52@gmail.com>
2018-07-27 16:42:27 -0400

    zombie

--- D11_Class_Methods_/zombies.rb
+++ D11_Class_Methods_/zombies.rb
@@ -21,13 +21,19 @@ class Zombie
   end
 
   def encounter
-          #if I outrun, return "im alive"
-          #else i dont outrun && survive_attack == false
+    if outrun_zombie? == true
+      p "You escaped unscathed!"
+    elsif survive_attack? == true && outrun_zombie? == false
+      p "You are now a zombie"
+      return @@horde << initialize(rand(@@max_speed), rand(@@max_strength))
+    else
+      p "You are zombie food"
+    end
   end
 
   def outrun_zombie?
-    zombie_meal_speed = Random.rand(6)
-    if zombie_meal_speed > speed
+    zombie_meal_speed = rand(@@max_speed)
+    if zombie_meal_speed > @speed
       return true
     else
       return false
@@ -35,12 +41,20 @@ class Zombie
   end
 
   def survive_attack?
-    zombie_meal_strength = Random.rand(9)
-    if zombie_meal_strength > strength
+    zombie_meal_strength = rand(@@max_strength)
+    if zombie_meal_strength > @strength
       return true
     else
       return false
     end
+  end
+
+  def self.plague_level
+    @@plague_level
+  end
+
+  def self.plague_level=(integer)
+    @@plague_level = integer
   end
 
   def self.all
@@ -54,21 +68,29 @@ class Zombie
   end
 
   def self.some_die_off
-    dead = rand(11)
-    @@horde.each do |die| # LOOP
-      @@horde
-
+    # dead = rand(11)
+      Random.rand(11).times do       # Each acts on each thing once, times acts a certain number of times.
+      @@horde.delete(@@horde.sample)
+      #@@horde.each do |die|
+      # LOOP
+    end
   end
 
   def self.spawn
-    @@horde << @@plague_level.rand(11)
-    @@max_speed.rand(6)
-    @@max_strength.rand(9)
+    rand(3..@@plague_level).times do
+      @@horde << self.new(rand(@@max_speed), rand(@@max_strength))
+    end
   end
-
+  #   @@plague_level.rand(11)
+  #   @@horde
+  #   # @@horde << @@plague_level.rand(11)
+  #   @@max_speed.rand(6)
+  #   @@max_strength.rand(9)
+  # end
   def self.increase_plague_level
-    @@plague_level += @@plague_level.rand(3)
-    return @@plague_level
+    @@plague_level += rand(4)
+    # @@plague_level += @@plague_level.rand(3)
+    # return @@plague_level
   end
 
 end
@@ -76,3 +98,17 @@ end
 p Zombie.all.inspect
 Zombie.new_day
 p Zombie.all.inspect
+zombie1 = Zombie.all[0]
+zombie2 = Zombie.all[1]
+zombie3 = Zombie.all[2]
+p zombie1.encounter
+p zombie2.encounter
+p zombie3.encounter
+Zombie.new_day
+p Zombie.all.inspect
+zombie1 = Zombie.all[0]
+zombie2 = Zombie.all[1]
+zombie3 = Zombie.all[2]
+p zombie1.encounter
+p zombie2.encounter
+p zombie3.encounter
